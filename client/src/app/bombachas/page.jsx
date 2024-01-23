866a9089764db8afeaad0a4c319f8a95e8a9e649
Constanzamw <cweiner92@gmail.com>
2024-01-23 17:20:09 -0300

    front2

--- client/src/app/bombachas/page.jsx
+++ client/src/app/bombachas/page.jsx
@@ -1,13 +1,40 @@
+"use client"
 import Link from "next/link";
 import Nav from "../../components/Nav/Nav";
-import { RiReplyLine } from "react-icons/ri";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+let baseUrl ="http://localhost:3001"
 
 export default function Bombachas() {
+
+  const [publications, setPublications]= useState([])
+
+  useEffect(()=>{
+    axios.get(`${baseUrl}/publications`)
+    .then( response => {
+      const bombachas = response.data.filter(publication => publication.cat === "BOMBACHAS")
+      setPublications(bombachas)
+    })
+    .catch(error => console.error('Error fetching publications:', error));
+  },[])
+
+
   return (
     <div>
-      <Nav />
-    <p> bombachas</p>
-      </div>
-   
+      <Nav/>
+      <h1>Publicaciones de Bombachas</h1>
+      <ul>
+        {publications.map(publication => (
+          <li key={publication.id}>
+            <h2>{publication.name}</h2>
+            <p>{publication.description}</p>
+            <p>Precio: {publication.price}</p>
+            <p>Talle: {publication.size}</p>
+            <img src={publication.image} alt={publication.name} style={{ maxWidth: '200px' }} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
--- client/src/app/enterizas/page.jsx
+++ client/src/app/enterizas/page.jsx
@@ -1,13 +1,40 @@
+"use client"
 import Link from "next/link";
 import Nav from "../../components/Nav/Nav";
-import { RiReplyLine } from "react-icons/ri";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Enterizas() {
+let baseUrl ="http://localhost:3001"
+
+export default function Enterizas () {
+
+  const [publications, setPublications]= useState([])
+
+  useEffect(()=>{
+    axios.get(`${baseUrl}/publications`)
+    .then( response => {
+      const bombachas = response.data.filter(publication => publication.cat === "ENTERIZAS")
+      setPublications(bombachas)
+    })
+    .catch(error => console.error('Error fetching publications:', error));
+  },[])
+
+
   return (
     <div>
-      <Nav />
-    <p> Enterizas</p>
-      </div>
-   
+      <Nav/>
+      <h1>Publicaciones de ENTERIZAS</h1>
+      <ul>
+        {publications.map(publication => (
+          <li key={publication.id}>
+            <h2>{publication.name}</h2>
+            <p>{publication.description}</p>
+            <p>Precio: {publication.price}</p>
+            <p>Talle: {publication.size}</p>
+            <img src={publication.image} alt={publication.name} style={{ maxWidth: '200px' }} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
