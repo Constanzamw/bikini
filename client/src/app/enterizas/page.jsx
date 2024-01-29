6b8328fd9c8837b5d7e13d2e416569410fb64c6a
Constanzamw <cweiner92@gmail.com>
2024-01-29 19:19:30 -0300

    front

--- client/src/app/enterizas/page.jsx
+++ client/src/app/enterizas/page.jsx
@@ -1,38 +1,52 @@
 "use client"
-import Link from "next/link";
+
 import Nav from "../../components/Nav/Nav";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Filters from "../../components/Filters/Filters";
 
 let baseUrl ="http://localhost:3001"
 
-export default function Enterizas () {
+export default function Enterizas() {
+  const [publications, setPublications] = useState([]);
 
-  const [publications, setPublications]= useState([])
-
-  useEffect(()=>{
+  useEffect(() => {
     axios.get(`${baseUrl}/publications`)
-    .then( response => {
-      const bombachas = response.data.filter(publication => publication.cat === "ENTERIZAS")
-      setPublications(bombachas)
-    })
-    .catch(error => console.error('Error fetching publications:', error));
-  },[])
-
+      .then(response => {
+        const enterizas = response.data.flatMap(item => {
+          // Filtrar por categoría "BOMBACHAS"
+          const filteredPublications = item.publications.filter(publication => publication.cat === "ENTERIZAS");
+          // Verificar si hay publicaciones antes de acceder a las propiedades
+          if (filteredPublications.length > 0) {
+            // Organizar los tamaños disponibles
+            const sizes = filteredPublications.map(publication => publication.size);
+            return {
+              name: item.name,
+              description: filteredPublications[0].description,
+              price: filteredPublications[0].price,
+              image: filteredPublications[0].image,
+              sizes: sizes
+            };
+          }
+          return null;
+        }).filter(Boolean); // Filtrar elementos nulos
+        setPublications(enterizas);
+      })
+      .catch(error => console.error('Error fetching publications:', error));
+  }, []);
 
   return (
     <div className="bg-gray-100 p-4">
       <Nav />
-      {/* <Filters /> */}
-      <h1 className="text-4xl font-bold mb-4 text-center text-pink-400/40"> ENTERIZAS</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <h1 className="text-4xl font-bold mb-4 text-center text-pink-400/40"> ENTERIZAS </h1>
+      <div>
         {publications.map(publication => (
-          <div key={publication.id} className="bg-white p-4 rounded-lg shadow-md">
+          <div key={publication.name} className="bg-white p-4 rounded-lg shadow-md mb-4">
             <h2 className="text-xl font-bold mb-2 text-black">{publication.name}</h2>
-            <p className="mb-2 text-black">{publication.description}</p>
+            {publication.description && <p className="mb-2 text-black">{publication.description}</p>}
             <img src={publication.image} alt={publication.name} className="max-w-full h-auto mb-2" />
             <p className="mb-2 text-black">Precio: {publication.price}</p>
-            <p className="mb-2 text-black">Talle: {publication.size}</p>
+            <p className="mb-2 text-black">Talles: {publication.sizes.sort((a, b) => a - b).join(', ')}</p>
           </div>
         ))}
       </div>
